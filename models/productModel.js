8e4e12fa3462ca402f79ec2ab67726a606c1e657
HogFanAndrew96 <andrewbarker96@gmail.com>
2024-02-08 15:25:10 -0600

    Changes to be committed: 	new file:   models/productModel.js 	modified:   package.json 	modified:   server.js

---
+++ models/productModel.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true,'Please enter a product name']
+    },
+    quantity: {
+        type: Number,
+        required: true, 
+        default: 0
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+}, 
+{
+    timestamps: true
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
--- server.js
+++ server.js
@@ -1,12 +1,41 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import {db_user, db_pw} from './secrets/dbConnection.js';
+import Product from './models/productModel.js';
+
 const app = express();
+
+app.use(express.json());
 
 //routes
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
     res.send('Hello NODE API');
 });
 
-app.listen(3000, () => {
-    console.log('Server Successfully Started on Port 3000');
+app.get('/product', async (req, res) => {
+    const products = await Product.find();
+    res.send(products);
+  });
+  
+app.post('/product', async(req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({error: error.message})
+    }
 });
+
+mongoose.set("strictQuery", false);
+mongoose.connect(`mongodb+srv://${db_user}:${db_pw}@cluster0.5646wb0.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log('Connected to MongoDB')
+        app.listen(3000, () => {
+            console.log('Server Successfully Started on Port 3000');
+        });
+    }).catch((err) => {
+        console.log('Error connecting to MongoDB', err);
+    });
